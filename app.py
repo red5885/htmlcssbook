7f0160d26ac3047f2a5bac28a0f67b2257c9b2b0
red5885 <red5885@garretson.home>
2021-05-22 15:33:15 -0500

    added init to pages to make it a package

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template
-from pages.views import *
+import pages
 
 app = Flask(__name__)
 app.config.from_pyfile('config.cfg')
@@ -9,5 +9,5 @@ def index():
     return render_template('index.html', pages = pages)
 
 app.add_url_rule('/', 'index', index)
-app.add_url_rule('/p20', 'p20', p20)
-app.add_url_rule('/p27', 'p27', p27)
+app.add_url_rule('/p20', 'p20', pages.views.p20)
+app.add_url_rule('/p27', 'p27', pages.views.p27)
